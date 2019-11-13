48a00f8d51a1145fe8b98811d2bc4630601af285
Alejandro Muñoz <admunoz261@misena.edu.co>
2019-11-13 10:53:33 -0500

    Se creo una nueva ventana

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 import './Global.css'
 import BadgeNew from './pages/BadgeNew'
+import Badges from './pages/Badges'
 
 
 const container = document.getElementById('app');
 
 
 // ReactDOM.render(__que__, __donde__)
-ReactDOM.render(<BadgeNew/>,container);
+ReactDOM.render(<Badges />,container);
