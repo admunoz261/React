f0d793de11749dfecaccbcd5d603bdda11294fb5
Alejandro Muñoz <admunoz261@misena.edu.co>
2019-12-12 10:56:57 -0500

    Se agrego todo sobre eliminar,(Funciona :V)

--- src/pages/BadgeDetailsContainer.js
+++ src/pages/BadgeDetailsContainer.js
@@ -9,12 +9,14 @@ class BadgeDetailsContainer extends React.Component {
     state = {
         loading: true,
         error: null,
-        data: undefined
+        data: undefined,
+        modalIsOpen: false
     }
 
     componentDidMount(){
         this.fetchData()
     }
+
 
     fetchData = async () => {
         this.setState({ loading: true, error: null })
@@ -26,6 +28,25 @@ class BadgeDetailsContainer extends React.Component {
             this.setState({ loading: false, data: data})
         }catch(error){
             this.setState({ loading: false, error: error})
+        }
+    }
+    handleOpenModal = e => {
+        this.setState({ modalIsOpen: true})
+    }
+
+    handleCloseModal = e => {
+        this.setState({ modalIsOpen: false})
+    }
+
+    handleDeleteBadge = async e => {
+        this.setState({ loading: true, error:null })
+        try {
+            await api.badges.remove(this.props.match.params.badgeId)
+            this.setState({ loading: false })
+            this.props.history.push('/badges')
+
+        }catch(error){
+            this.setState({ loading: false, error: error })
         }
     }
     
@@ -41,7 +62,13 @@ class BadgeDetailsContainer extends React.Component {
         const badge = this.state.data
 
         return (
-            <BadgeDetails badge={this.state.data}/>
+            <BadgeDetails 
+                onCloseModal={this.handleCloseModal}
+                onOpenModal={this.handleOpenModal}
+                modalIsOpen={this.state.modalIsOpen}
+                onDeleteBadge={this.handleDeleteBadge}
+                badge={this.state.data}
+            />
         );
     }
 }
